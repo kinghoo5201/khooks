c1751e7ad29a4b0531102126269c2c794b31bac9
xiaohu.hjh <huangjinhu@come-future.com>
2024-01-24 16:48:02 +0800

    fix: 修复防抖与async问题

--- src/hooks/use-unique-async.ts
+++ src/hooks/use-unique-async.ts
@@ -1,7 +1,7 @@
 // UseUniqueAsync
 // @description 防止重复请求、防止组件销毁后执行setState、支持防抖
 // @author kinghoo
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import debounce from 'lodash/debounce';
 import isNil from 'lodash/isNil';
 
@@ -18,30 +18,36 @@ export function useUniqueAsync<Parameter extends any[], Result extends any>(
   $fn.current = fn;
   const [response, setResponse] = useState<Result>();
 
-  const asyncFn = useCallback(
-    (() => {
-      const fnBody = async (...args: Parameter) => {
-        try {
-          setLoading(true);
-          const requestId = Date.now();
-          reqId.current = requestId;
-          const res = await $fn.current(...args);
-          if (reqId.current !== requestId || isunmount.current) return;
-          setLoading(false);
-          setResponse(res);
-          return res;
-        } catch (e) {
-          setLoading(false);
-          throw e;
-        }
-      };
-      if (isNil(debounceTime) || debounceTime < 0) {
-        return fnBody;
+  const asyncFn = useMemo(() => {
+    const fnBody = async (...args: Parameter) => {
+      try {
+        setLoading(true);
+        const requestId = Date.now();
+        reqId.current = requestId;
+        const res = await $fn.current(...args);
+        if (reqId.current !== requestId || isunmount.current) return;
+        setLoading(false);
+        setResponse(res);
+        return res;
+      } catch (e) {
+        setLoading(false);
+        throw e;
       }
-      return debounce(fnBody, debounceTime);
-    })(),
-    [],
-  );
+    };
+    if (isNil(debounceTime) || debounceTime < 0) {
+      return fnBody;
+    }
+    return debounce((...args: Parameter) => {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const res = await fnBody(...args);
+          resolve(res);
+        } catch (e) {
+          reject(e);
+        }
+      });
+    }, debounceTime);
+  }, []);
 
   useEffect(
     () => () => {
